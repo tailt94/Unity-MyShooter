f0203a6665840a79919d49fcc0d34ff858766df3
TuanTai <tuantai0625@gmail.com>
2017-01-14 02:31:38 +0700

    Add enemy shoot bullets

---
+++ Assets/Scripts/EnemyGun.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGun : MonoBehaviour {
+	public GameObject EnemyBulletGO;
+	// Use this for initialization
+	void Start () {
+		Invoke ("FireBullet", 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FireBullet() {
+		GameObject playerShip = GameObject.Find ("PlayerGO");
+
+		if (playerShip != null) {
+			GameObject bullet = (GameObject)Instantiate (EnemyBulletGO);
+
+			//set bullet initial position
+			bullet.transform.position = transform.position;
+
+			//calculate direction toward player ship
+			Vector2 direction = playerShip.transform.position - bullet.transform.position;
+
+			//set bullet position
+			bullet.GetComponent<EnemyBullet>().SetDirection(direction);
+		}
+	}
+}
