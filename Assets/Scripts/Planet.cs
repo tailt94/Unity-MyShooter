78c96d1e0f4056a439a3efe6544db6647c2699bd
TuanTai <tuantai0625@gmail.com>
2017-01-15 03:19:04 +0700

    Add planet scroll

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour {
+	public float speed;
+	public bool isMoving; //flag to make planet scroll down
+
+	Vector2 min;
+	Vector2 max;
+
+	void Awake() {
+		isMoving = false;
+		min = Camera.main.ViewportToWorldPoint (new Vector2 (0, 0));
+		max = Camera.main.ViewportToWorldPoint (new Vector2 (1, 1));
+
+		//add planet sprite half height to max y
+		max.y = max.y + GetComponent<SpriteRenderer>().sprite.bounds.extents.y;
+
+		//subtract the planet sprite half height to min y
+		min.y = min.y - GetComponent<SpriteRenderer>().sprite.bounds.extents.y;
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isMoving) {
+			return;
+		}
+
+		//get current position of planet
+		Vector2 position = transform.position;
+
+		//Calculate new position
+		position = new Vector2(position.x, position.y + speed * Time.deltaTime);
+
+		//Update planet position;
+		transform.position = position;
+
+		//If planet gets to min y position, top moving planet
+		if (transform.position.y < min.y) {
+			isMoving = false;
+		}
+	}
+
+	public void ResetPosition() {
+		//reset position of planet to random x, max y
+		transform.position = new Vector2(Random.Range(min.x, max.x), max.y);
+	}
+}
---
+++ Assets/Scripts/PlanetController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetController : MonoBehaviour {
+	public GameObject[] Planets;
+
+	//Queue to hold planets
+	Queue<GameObject> availablePlanets = new Queue<GameObject>();
+	// Use this for initialization
+	void Start () {
+		//add planets to Queue
+		availablePlanets.Enqueue(Planets[0]);
+		availablePlanets.Enqueue(Planets[1]);
+		availablePlanets.Enqueue(Planets[2]);
+
+		//call MovePlaneDown every 20s
+		InvokeRepeating("MovePlanetDown", 0, 20f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//Function to dequeue a planet, and set isMoving flag to true
+	//so that the planet starts scrolling down
+	void MovePlanetDown() {
+		//return if the queue is empty
+		if (availablePlanets.Count == 0) {
+			return;
+		}
+
+		//get a planet from the queue
+		GameObject aPlanet = availablePlanets.Dequeue();
+
+		//set the planet isMoving flag to true
+		aPlanet.GetComponent<Planet>().isMoving = true;
+	}
+
+	//Enqueue planets that are below screen and are not moving
+	void EnqueuPlanets() {
+		foreach (GameObject aPlanet in Planets) {
+			//if the planet if below the screen, and the planet is not moving
+			if ((aPlanet.transform.position.y < 0) && (!aPlanet.GetComponent<Planet> ().isMoving)) {
+				//reset planet position
+				aPlanet.GetComponent<Planet>().ResetPosition();
+
+				//Enqueue planet	
+				availablePlanets.Enqueue(aPlanet);
+			}
+		}
+	}
+}
